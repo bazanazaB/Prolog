df90363bcf665b736d67c042ba6a84db17472fab
bazanazaB <bazankod3@gmail.com>
2025-04-02 11:54:55 -0600

    Avance ejercicios de prolog

---
+++ guardianes.pl
@@ -0,0 +1,18 @@
+guardian(apolo).
+guardian(hecate).
+guardian(ares).
+guardian(hermes).
+
+templo(fuego).
+templo(agua).
+templo(tierra).
+templo(aire).
+
+no_cuida(apolo, fuego).
+no_cuida(apolo, tierra).
+no_cuida(hecate, aire).
+no_cuida(ares, fuego).
+no_cuida(ares, agua).
+cuida(hermes, fuego).
+cuida(hermes, agua).
+
